b05ec82764ed7fbc43731c8781056a741d0fb5ba
The Toasta <nolanp678@gmail.com>
2023-11-19 00:59:00 +0000

    e

---
+++ commands/fun/ps.js
@@ -0,0 +1,374 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ps')
+		.setDescription('ps - OWNER LIMITED'),
+	async execute(interaction) {
+		await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+
+        await interaction.reply('<@508105003035131914>');
+        await interaction.reply('<@508105003035131914>');
+
+	},
+};
+
+console.log('HEHHE')
